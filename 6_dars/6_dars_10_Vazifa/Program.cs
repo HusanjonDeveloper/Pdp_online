43987b1b1f684d357c02abc6ad880ae08b3cba95
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:14:57 +0500

    6_dars_10_Vazifa

---
+++ 6_dars/6_dars_10_Vazifa/Program.cs
@@ -0,0 +1,17 @@
+﻿// 10-vazifa
+// N sonini M soniga bo'lgandagi qoldiqni, / va % operatorlarini ishlatmasdan topuvchi dastur tuzing. 
+
+using System;
+class pdp_online
+{
+    static int gt(int num, int d)
+    {
+        return (num - d * (num / d));
+    }
+    public static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine(gt(n, m));
+    }
+}
