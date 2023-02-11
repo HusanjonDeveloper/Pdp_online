748374b8857b985c4a731806cf66384ee3b9a23e
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:12:41 +0500

    6_dars_8_Vazifa

---
+++ 6_dars/6_dars_8_Vazifa/Program.cs
@@ -0,0 +1,4 @@
+﻿// 8-vazifa
+// Chang’ichi birinchi kun 10 km yo’l bosdi. Har kungi bosgan yolini kechagisidan
+// 10% oshirib borsa nechanchi kun bir kunda 20 km dan kam bo’lmagan yo’lni bosadi
+// va nechchi kunda barcha kungi bosgan yo’li summasi 100 km dan oshadi.
