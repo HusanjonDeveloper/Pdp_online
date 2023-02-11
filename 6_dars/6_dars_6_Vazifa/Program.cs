86288bdffe6ca619283a78cd92f4a334802f9260
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:09:29 +0500

    6_dars_6_Vazifa

---
+++ 6_dars/6_dars_6_Vazifa/Program.cs
@@ -0,0 +1,23 @@
+﻿// 6-vazifa
+
+// Raqamlar yig’indisi 15ga teng bo’lgan 100-500 oraliqdagi sonlarni ekranga chiqazuvchi dastur tuzing.
+
+
+using System;
+
+class vazifa5
+{
+    public static void Main(String[] args)
+    {
+        int sum = 0;
+        for (int n = 100; n < 500; n++)
+        {
+            sum = n % 10 + (n / 10) % 10 + (n / 100);
+            if (sum == 15)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+    }
+}
