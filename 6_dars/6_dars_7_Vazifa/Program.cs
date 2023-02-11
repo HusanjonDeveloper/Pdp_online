073412e8fafdcd53c9b8be981771bd9032bd732f
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:11:26 +0500

    6_dars_7_Vazifa

---
+++ 6_dars/6_dars_7_Vazifa/Program.cs
@@ -0,0 +1,11 @@
+﻿// 7-vazifa
+// To’rt xonali sonlar orasidan 47 ga bo’lganda qoldiq
+// 43 va 43 ga bo’lganda qoldiq 40 bo’lgan sonlarni topish dasturini tuzing.
+
+for (int i = 1000; i < 10000; i++)
+{
+    if (i % 47 == 43 && i % 43 == 40)
+    {
+        Console.WriteLine(i);
+    }
+}
