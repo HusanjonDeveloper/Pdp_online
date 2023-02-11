fd3cd367c8095047fcc48488aa9570530321adb3
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:06:28 +0500

    6_dars_4_Vazifa

---
+++ 6_dars/6_dars_4_Vazifa/Program.cs
@@ -0,0 +1,36 @@
+﻿// n butun soni berilgan, 1 dan n sonigacha bo'lgan
+// tub sonlarni ekranda chiqaruvchi dastur tuzing. n > 1
+// Kiritish: 5
+// Chiqish: 2 3
+
+int num, i, ctr, stno, enno;
+
+Console.Write("\n\n");
+Console.Write("Raqamlar diapazonidagi tub sonlarni toping:\n");
+Console.Write("---------------------------------------------------");
+Console.Write("\n\n");
+
+Console.Write("Diapazonning boshlang'ich raqamini kiriting: ");
+stno = Convert.ToInt32(Console.ReadLine());
+Console.Write("Diapazonning yakuniy sonini kiriting: ");
+enno = Convert.ToInt32(Console.ReadLine());
+Console.Write("{0} va {1} orasidagi tub sonlar: \n", stno, enno);
+
+for (num = stno; num <= enno; num++)
+{
+    ctr = 0;
+
+    for (i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            ctr++;
+            break;
+        }
+    }
+
+    if (ctr == 0 && num != 1)
+        Console.Write("{0} ", num);
+}
+Console.Write("\n");
+
