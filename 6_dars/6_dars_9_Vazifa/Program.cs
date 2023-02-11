bf4e0a91ce81c11d656eae6fce4162fe2904c039
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:13:52 +0500

    6_dars_9_Vazifa

---
+++ 6_dars/6_dars_9_Vazifa/Program.cs
@@ -0,0 +1,15 @@
+﻿// 9-vazifa
+// 30 - 100 oraliqdagi 3 ga karrali va oxirgi raqami 2,4,8 bo’lgan sonlarni ekranga chiqazing.
+
+for (int i = 30; i < 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        if (i % 10 == 2)
+            Console.WriteLine(i);
+        if (i % 10 == 4)
+            Console.WriteLine(i);
+        if (i % 10 == 8)
+            Console.WriteLine(i);
+    }
+}
