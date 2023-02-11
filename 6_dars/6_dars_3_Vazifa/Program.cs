feba42bff153f9fae1563279b259fc51369cfb87
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:03:19 +0500

    6_dars_3_Vazifa

---
+++ 6_dars/6_dars_3_Vazifa/Program.cs
@@ -0,0 +1,25 @@
+﻿// n butun soni berilgan, 1 dan n sonigacha bo'lgan toq
+// sonlarni ekranda chiqaruvchi dastur tuzing. n > 1
+// Kiritish: 5
+// Chiqish: 3
+
+
+using System;
+
+internal class Programm
+{
+    private static void Main(string[] args)
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("num");
+            int num = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < num)
+            {
+                Console.WriteLine(counter);
+                counter += 2;
+            }
+        }
+    }
+}
