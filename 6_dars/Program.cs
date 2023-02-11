ca9637b473c1dc8417bfb4a87595cbc9d274d453
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 21:54:32 +0500

    6_dars_1_vazifa

---
+++ 6_dars/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1-vazifa
+// K va N butun soni berilgan. K sonini N marta ekranda chiqaruvchi dastur tuzing.
+// Kiritish: 
+// K: 2
+// N: 3
+// Chiqish: 2 2 2 
+
+System.Console.WriteLine("k");
+int k = int.Parse(Console.ReadLine());
+System.Console.WriteLine("n");
+int n = int.Parse(Console.ReadLine());
+
+for (int c = 1; c <= k; c++)
+{
+    System.Console.WriteLine(n);
+}
