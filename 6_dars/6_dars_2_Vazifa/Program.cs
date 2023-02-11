81cac72e7fe1844aa8d5e611d7b8d8d579e6f8be
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 21:59:36 +0500

    6_dars_2_Vazifa

---
+++ 6_dars/6_dars_2_Vazifa/Program.cs
@@ -0,0 +1,25 @@
+﻿// num1 va num2 butun sonlari berilgan, num1 dan num2 gacha bo'lgan sonlar
+// yig'indisini topuvchi dastur tuzing. num2 > num1, (num1, num2)
+// Kiritish:
+// num1: 2 ;
+// num2: 4 ;
+// chiqish: 6 ;
+
+internal class Programm
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+        Console.WriteLine("birinchi sonni kiritng: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("ikkinchi sonni kiriting: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        for (num1 = 1; num1 <= num2; num1++)
+        {
+            sum += num1;
+        }
+        System.Console.WriteLine(sum);
+
+    }
+}
