1cddaa60e6ff977a1d0e567b16f245c059e21679
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 22:20:36 +0500

    1_dars_2_Vazifa

---
+++ 1_Dars_2_Vazifa/Program.cs
@@ -0,0 +1,5 @@
+﻿// 2-vazifa
+//Mening birinchi dasturim nomi ostida yangi console
+//ko'rinishidagi proyekt hosil qiling. Ekranda aynan 
+// "Mening birinchi dasturim" satrini ko'rsatib beruvchi ketma-ketlikni 
+// dasturda mujassamlashtiring va natijasini screenshot qilib yuboring
